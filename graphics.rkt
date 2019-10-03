a174f7dd05461c01c29d2c13cc4456740da8c53f
lkh01 <lcntrpn@gmail.com>
2019-10-03 20:51:38 +0200

    add command line arguments

--- graphics.rkt
+++ graphics.rkt
@@ -10,28 +10,34 @@
 (define gui%
   (class object%
     (init-field screen-width
-                screen-height)
+                screen-height
+                map-width
+                map-height
+                iterations)
     (define frame
       (new frame%
            [label "procsland"]
            [style '(no-resize-border)]))
-    (define tm (generate-map 45 39 0 '()))
+    (define tm (generate-map map-width map-height 0 '()))
     (define (final-map iter fm)
-      (if (= iter 16)
-          (cellular-automaton convert-to-beach fm 45 39 0 '())
-          (final-map (+ iter 1) (cellular-automaton cell-decide fm 45 39 0 '()))))
+      (if (= iter iterations)
+          (cellular-automaton convert-to-beach fm map-width map-height 0 '())
+          (final-map (+ iter 1) (cellular-automaton cell-decide fm map-width map-height 0 '()))))
     (define fm (final-map 0 tm))
     (define canvas
       (new canvas%
            [parent frame]
            [min-width screen-width]
            [min-height screen-height]
-           [paint-callback (draw-map fm 45 39)]))
+           [paint-callback (draw-map fm map-width map-height)]))
     
     (send frame show #t)
     (super-new)))
 
-(define (make-gui w h)
+(define (make-gui w h mw mh i)
   (new gui%
        [screen-width w]
-       [screen-height h]))
+       [screen-height h]
+       [map-width mw]
+       [map-height mh]
+       [iterations i]))
--- main.rkt
+++ main.rkt
@@ -1,3 +1,4 @@
+#!/usr/bin/env racket
 #lang racket
 
 (module+ test
@@ -9,6 +10,43 @@
 
 (module+ main
   (require "graphics.rkt")
-  (make-gui 640 400)
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
+
+  ; command line args
+  (define screen-width (make-parameter 1600))
+  (define screen-height (make-parameter 900))
+  (define map-width (make-parameter 45))
+  (define map-height (make-parameter 39))
+  (define iterations (make-parameter 16))
+
+  (define parser
+    (command-line
+      #:usage-help
+      "procsland is an hex map island generator"
+      "based on cellular automata."
+
+      #:once-each
+      [("-W" "--screen-width") SCREEN-WIDTH
+                               "width of the screen in pixels"
+                               (screen-width (string->number SCREEN-WIDTH))]
+      [("-H" "--screen-height") SCREEN-HEIGHT
+                                "height of the screen in pixels"
+                                (screen-height (string->number SCREEN-HEIGHT))]
+      [("-x" "--map-width") MAP-WIDTH
+                            "width of the map in (hex) tiles"
+                            (map-width (string->number MAP-WIDTH))]
+      [("-y" "--map-height") MAP-HEIGHT 
+                             "height of the map in (hex) tiles"
+                             (map-height (string->number MAP-HEIGHT))]
+      [("-i" "--iterations") ITERATIONS 
+                             "number of iterations of the cellular automata to perform"
+                             (iterations (string->number ITERATIONS))]
+
+      #:args () (void)))
+
+  (make-gui 
+    (screen-width)
+    (screen-height)
+    (map-width)
+    (map-height)
+    (iterations))
   )
