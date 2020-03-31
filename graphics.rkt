a0df1e66d01c60324dd8ced11cbd3ed5a4a56b6f
hgluka <luka.hadzi@pm.me>
2020-03-31 12:15:35 +0200

    draw empty frame in graphics.rkt

--- graphics.rkt
+++ graphics.rkt
@@ -19,8 +19,6 @@
 (define beach-bitmap
   (read-bitmap "images/hex_beach_large.png" 'png/alpha))
 
-(struct point ([x : Integer] [y : Integer]))
-
 (define hex-width
   (send land-bitmap get-width))
 
@@ -38,19 +36,11 @@
       (new frame%
            [label "procsland"]
            [style '(no-resize-border)]))
-    (define tm (generate-map map-width map-height 0 '()))
-    (define (final-map iter fm)
-      (if (= iter iterations)
-          (automaton beachify fm map-width map-height 0 '())
-          (final-map (+ iter 1) (automaton turn fm map-width map-height 0 '()))))
-    (define fm (final-map 0 tm))
     (define canvas
       (new canvas%
            [parent frame]
            [min-width screen-width]
-           [min-height screen-height]
-           [paint-callback (draw-map fm map-width map-height)]))
-    
+           [min-height screen-height]))
     (send frame show #t)
     (super-new)))
 
