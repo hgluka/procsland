a33eed560a33b0342d1a257a937202a4b8177235
Luka Hadzi-Djokic <luka.hadzi@pm.me>
2022-02-23 17:42:57 +0100

    add focus and movement

--- graphics.rkt
+++ graphics.rkt
@@ -27,34 +27,69 @@
      (+ (point-x (offset r)) (* c hex-width))
      (* r (point-y (offset r))))))
 
-(: draw-map (-> (Array Symbol) (-> (Instance Canvas%) (Instance DC<%>) Void)))
-(define (draw-map tile-map)
-  (λ (canvas dc)
-    (array-map
-     (λ: ([js : (Vectorof Index)])
-       (let ([s : Symbol (array-ref tile-map js)]
-             [p : point (tile-pos js)])
-         (cond
-           [(equal? s 'land) (begin
-                               (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height))]
-           [(equal? s 'water) (begin
-                                (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 3 hex-width) (* 1 hex-height) hex-width hex-height))]
-           [(equal? s 'beach) (begin
-                                (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 3 hex-width) (* 3 hex-height) hex-width hex-height))]
-           [(equal? s 'mountain) (begin
-                                   (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
-                                   (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 0 (random 2)) hex-width) (* (+ 1 (random 2)) hex-height) hex-width hex-height))]
-           [(equal? s 'forest) (begin
+(: draw-map (-> (Array Symbol) (Instance Canvas%) (Instance DC<%>) Void))
+(define (draw-map tile-map canvas dc)
+  (array-map
+   (λ: ([js : (Vectorof Index)])
+     (let ([s : Symbol (array-ref tile-map js)]
+           [p : point (tile-pos js)])
+       (cond
+         [(equal? s 'land) (begin
+                             (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height))]
+         [(equal? s 'water) (begin
+                              (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 3 hex-width) (* 1 hex-height) hex-width hex-height))]
+         [(equal? s 'beach) (begin
+                              (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 3 hex-width) (* 3 hex-height) hex-width hex-height))]
+         [(equal? s 'mountain) (begin
                                  (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
-                                 (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 1 (random 2)) hex-width) (* 0 hex-height) hex-width hex-height))]
-           [(equal? s 'farm) (begin
+                                 (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 0 1) hex-width) (* (+ 1 0) hex-height) hex-width hex-height))]
+         [(equal? s 'forest) (begin
                                (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
-                               (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 2 hex-width) (* 3 hex-height) hex-width hex-height))]
-           [(equal? s 'city) (begin
-                               (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
-                               (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 0 (random 2)) hex-width) (* 3 hex-height) hex-width hex-height))])))
-     (indexes-array (array-shape tile-map)))
-    (void)))
+                               (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 1 0) hex-width) (* 0 hex-height) hex-width hex-height))]
+         [(equal? s 'farm) (begin
+                             (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
+                             (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 2 hex-width) (* 3 hex-height) hex-width hex-height))]
+         [(equal? s 'city) (begin
+                             (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* 0 hex-width) (* 0 hex-height) hex-width hex-height)
+                             (send dc draw-bitmap-section tileset-bitmap (point-x p) (point-y p) (* (+ 0 0) hex-width) (* 3 hex-height) hex-width hex-height))])))
+   (indexes-array (array-shape tile-map)))
+  (void))
+
+(define game-canvas%
+  (class canvas%
+    (init-field [focus : (Mutable-Vectorof Integer)]
+                [tm : (Array Symbol)]
+                [tm-width : Integer]
+                [tm-height : Integer])
+    (inherit refresh)
+    (define/override (on-char ke)
+      (case (send ke get-key-release-code)
+        [(left) (begin
+                 (vector-set! focus 1 (max 0 (- (vector-ref focus 1) 1)))
+                 (refresh))]
+        [(right) (begin
+                  (vector-set! focus 1 (min tm-width (+ (vector-ref focus 1) 1)))
+                  (refresh))]
+        [(up) (begin
+               (vector-set! focus 0 (max 0 (- (vector-ref focus 0) 1)))
+               (refresh))]
+        [(down) (begin
+                 (vector-set! focus 0 (min tm-height (+ (vector-ref focus 0) 1)))
+                 (refresh))]
+        [else (void)]))
+
+    (: game-paint-callback (-> (Instance Canvas%) (Instance DC<%>) Void))
+    (define/private (game-paint-callback c dc)
+                    (draw-map tm c dc)
+                    (let ([inds (indexes-array (array-shape tm))])
+                      (send dc draw-bitmap-section tileset-bitmap
+                            (point-x (tile-pos (array-ref inds focus)))
+                            (point-y (tile-pos (array-ref inds focus)))
+                            (* 0 hex-width) (* 5 hex-height)
+                            hex-width hex-height))
+                    (void))
+    (super-new (paint-callback (lambda (c dc) (game-paint-callback c dc))))))
+
 
 (define gui%
   (class object%
@@ -75,12 +110,19 @@
            [style '(no-resize-border)]))
     (: tm (Array Symbol))
     (define tm (generate-map map-height map-width iterations land-mass mountain-mass forest-mass beach-mass city-mass))
+
+    (: focus (Mutable-Vectorof Integer))
+    (define focus (vector 0 0))
+
     (: canvas (Instance Canvas%))
     (define canvas
-      (new canvas%
+      (new game-canvas%
+           [focus focus]
+           [tm tm]
+           [tm-width (- map-width 1)]
+           [tm-height (- map-height 1)]
            [parent frame]
            [min-width screen-width]
-           [min-height screen-height]
-           [paint-callback (draw-map tm)]))
+           [min-height screen-height]))
     (send frame show #t)
     (super-new)))
